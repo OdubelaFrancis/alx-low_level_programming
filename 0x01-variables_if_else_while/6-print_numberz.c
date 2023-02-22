8abcd6df01fb72ae70557722e2da30f425c8948c
OdubelaFrancis <francisolalekanodubela@yahoo.com>
2023-02-22 04:45:34 -0800

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,5 +3,20 @@
 #include <time.h>
 /**
  * main - main function to generate a random number
- * Description: print all single digit number of base 10Success
+ * Description: print all single digit number of base 10
+ * starting from 0, followed by new line.
  * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch = 0;
+
+	while (ch < 10)
+	{
+		putchar(48 + ch);
+		ch++;
+	}
+
+	putchar('\n');
+	return (0);
+}
