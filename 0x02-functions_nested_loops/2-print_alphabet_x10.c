daa4c235bd99badc8a9df737e8dd6a3818349d68
OdubelaFrancis <francisolalekanodubela@yahoo.com>
2023-02-22 11:08:04 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,24 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_alphabet_x10 - prints the alphabet in lowercase x10.
  *
- * Return: Always 0.
+ * Return: nothing on success.
  */
-int main(void)
+void print_alphabet_x10(void)
 {
-    print_alphabet_x10();
-    return (0);
+	int start = 97, end = 122, count = 1;
+
+	while (count <= 10)
+	{
+		while (start <= end)
+		{
+			_putchar(start);
+			start++;
+		}
+		_putchar('\n');
+
+		start = 97;
+		count++;
+	}
 }
